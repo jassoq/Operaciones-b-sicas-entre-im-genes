a4bddd6b4bd929362c6b3f4a5e2f94f6b037c2c2
jassoq <104932844+jassoq@users.noreply.github.com>
2022-05-04 19:08:04 -0500

    Add files via upload

---
+++ Suma.py
@@ -0,0 +1,68 @@
+'''
+import cv2
+import numpy as np
+from ctypes import c_float
+
+def norm(c_float):
+    c_float = c_float - np.min(c_float)
+    c_float = c_float / np.max(c_float)
+    return c_float
+
+a = cv2.imread("IMG1.jpg")
+b = cv2.imread("IMG2.jpg")
+a_float = a.astype (np.int32)
+b_float = b.astype (np.int32)
+
+#SUMA
+suma = a_float + b_float
+suma = norm (suma)
+suma = suma * 255
+su = suma.astype(np.uint8)
+cv2.imshow("SUMA",su)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+'''
+
+
+import cv2
+
+#Se leen las imagenes
+img1 = cv2.imread('IMG1.jpg')
+img2 = cv2.imread('IMG2.jpg')
+
+# utilizamos la función cv2.add, en ella especificamos img1 e img2 correspondientes a las imágenes leídas.
+#IMG = cv2.add(img1,img2)
+
+IMG= img1 + img2
+
+#Visualizamos los resultados
+cv2.imshow('IMG',IMG)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+###
+'''
+import cv2
+
+# donde estamos leyendo directamente en escala de grises al ubicar 0
+img1 = cv2.imread('IMG1.jpg',0)
+img2 = cv2.imread('IMG2.jpg',0)
+
+IMG = cv2.add(img1,img2)
+
+#usamos print para obtener el primer elemento de cada imagen
+print('img1[0,0]= ', img1[0,0])
+print('img2[0,0]= ', img2[0,0])
+print('IMG[0,0]= ', IMG[0,0])
+
+cv2.imshow('IMG',IMG)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#Entonces si sumamos 65 y 226, tenemos un total de 291, sin embargo, 
+#vemos que en resA[0,0] obtenemos 255, esto es debido a que al usar cv2.add si la suma excede a 255, el valor vuelve a 255.
+'''
